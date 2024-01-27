fa484e6188bb703ddb7d7907e7d53ff18507960b
Arthur Teixeira <arthursony1@hotmail.com>
2024-01-27 14:34:25 -0300

    req 1 e 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    count = 0
+
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+
+        if i[0] <= target_time <= i[1]:
+            count += 1
+
+    return count
