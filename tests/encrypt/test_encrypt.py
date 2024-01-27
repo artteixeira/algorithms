f03d71d829e9cbf10bd13741ba54d7e6b9323d72
Arthur Teixeira <arthursony1@hotmail.com>
2024-01-27 14:44:34 -0300

    fix path tests - req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("socorro", "socorro")
+
+    with pytest.raises(TypeError):
+        encrypt_message(123, 123)
+
+    assert encrypt_message("Cachorro", 6) == "or_rohcaC"
+
+    assert encrypt_message("Cachorro", 7) == "rrohcaC_o"
+
+    assert encrypt_message("Cachorro", 1) == "C_orrohca"
